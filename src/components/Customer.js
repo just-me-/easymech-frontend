8791aca4b375c3ab6bc12c2f4fc55c33afb21ab2
just-me- <hess-marcel@bluewin.ch>
2019-04-06 22:25:06 +0200

    Erfolgreiche Auslagerung

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,7 +1,9 @@
 // @flow
 
-import React, { useState } from 'react'
-import { Button, Grid, Header, Form, Input } from 'semantic-ui-react'
+import React, {  } from 'react'
+import { Button, Header, Form } from 'semantic-ui-react'
+
+import CustomerFields from "./CustomerFields";
 
 function Customer() {
   //const [msg, setMsg] = useState("");
@@ -11,62 +13,7 @@ function Customer() {
         Kunde (erstellen)
       </Header>
       <Form>
-        <Header as='h2'>Firma</Header>
-        <div className="Form-section">
-          <Form.Group widths='equal'>
-            <Form.Input
-              id='company_name'
-              label='Name'
-            />
-            <Form.Input
-              id='company_address'
-              label='Adresse'
-            />
-          </Form.Group>
-
-          <Form.Group widths='equal'>
-            <Form.Input
-              id='company_zip'
-              label='PLZ'
-            />
-            <Form.Input
-              id='company_location'
-              label='Ort'
-            />
-          </Form.Group>
-        </div>
-
-        <Header as='h2'>Kontakt</Header>
-        <div className="Form-section">
-          <Form.Group widths='equal'>
-            <Form.Input
-              id='contact_firstname'
-              label='Vorname'
-            />
-            <Form.Input
-              id='contact_lastname'
-              label='Nachname'
-            />
-          </Form.Group>
-
-          <Form.Group widths='equal'>
-            <Form.Input
-              id='contact_mail'
-              label='E-Mail'
-            />
-            <Form.Input
-              id='contact_phone'
-              label='Tel.'
-            />
-          </Form.Group>
-
-          <Form.Group widths='equal'>
-            <Form.Input
-              id='contact_notes'
-              label='Notizen'
-            />
-          </Form.Group>
-        </div>
+        <CustomerFields/>
         <Button primary content='Speichern' icon='save' labelPosition='left' />
       </Form>
     </div>
