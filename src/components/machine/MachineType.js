bdde5f0fa8bc1aef043af1460ee73be8b45ae1f9
udfu <dario_delpiero10@hotmail.com>
2019-04-18 14:35:29 +0200

    added different view states (list, search, add)

--- src/components/machine/MachineType.js
+++ src/components/machine/MachineType.js
@@ -10,19 +10,10 @@ function MachineType() {
 
     const [machineTypeData, setMachineTypeData] = useState({});
     const [formIsValid, setFormIsValid] = useState(false);
+    const [viewState, setViewState] = useState('list');
 
     function addMachineType() {
         if(formIsValid) {
-            /*api
-                .addCustomer(machineTypeData)
-                .then((result) => {
-                    result = api.checkResponse(result);
-                    NotificationManager.success("Der Kunde wurde erfolgreich gespeichert.", result.firma+" erfasst");
-                })
-                .catch(error => {
-                    console.log("Ups, ein Fehler ist aufgetreten", error);
-                    NotificationManager.error("Beim Speichern ist ein Fehler aufgetreten.", "Bitte erneut versuchen!");
-                });*/
             console.log(machineTypeData)
         } else {
             if(!formIsValid){
@@ -33,16 +24,61 @@ function MachineType() {
 
     return (
         <div>
-            <Header as='h1' textAlign='center'>
-                Maschinentyp erfassen
-            </Header>
-            <Form>
-                <MachineTypeFields setData={setMachineTypeData} setValidState={setFormIsValid} />
-                <Button primary content='Speichern' icon='save' labelPosition='left' floated='right'
-                        onClick={() => addMachineType()}
+            {viewState === 'search' &&
+            <div>
+                <Header as='h1' textAlign='center'>
+                    Maschinentyp suchen
+                </Header>
+                <Form>
+                    <Button primary content='Suchen' icon='search' labelPosition='left'
+                            onClick={() => setViewState('search')} floated='right'
+                    />
+                    <Button content='Zurück' icon='arrow left' labelPosition='left'
+                            onClick={() => setViewState('list')}
+                    />
+                </Form>
+            </div>
+            }
+
+            {viewState === 'list' &&
+            <div>
+                <Header as='h1' textAlign='center'>
+                    Maschinentypen anzeigen
+                </Header>
+                <p>Liste mit Maschinentypen....</p>
+                <p>Liste mit Maschinentypen....</p>
+                <p>Liste mit Maschinentypen....</p>
+                <Button content='Neuen Maschninentyp erfassen' icon='add circle' labelPosition='left'
+                        onClick={() => setViewState('add')}
                 />
-            </Form>
-            <Notification/>
+                <Button primary content='Suchen' icon='search' labelPosition='left'
+                        onClick={() => setViewState('search')} floated='right'
+                />
+            </div>
+            }
+
+
+            {viewState === 'add' &&
+            <div>
+                <Header as='h1' textAlign='center'>
+                    Maschinentyp erfassen
+                </Header>
+
+                <Form>
+                    <MachineTypeFields setData={setMachineTypeData} setValidState={setFormIsValid}/>
+                    <Button content='Zurück' icon='arrow left' labelPosition='left'
+                            onClick={() => setViewState('list')}
+                    />
+                    <Button primary content='Speichern' icon='save' labelPosition='left' floated='right'
+                            onClick={() => {
+                                addMachineType();
+                                setViewState('list')
+                            }}
+                    />
+                </Form>
+                <Notification/>
+            </div>
+            }
         </div>
     )
 
