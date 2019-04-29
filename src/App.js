394df86180e9394cfadf08f859891871936badc2
udfu <dario_delpiero10@hotmail.com>
2019-04-29 15:05:13 +0200

    keycloak never used...

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Grid } from 'semantic-ui-react'
-import Keycloak from 'keycloak-js';
+/*import Keycloak from 'keycloak-js';*/
 
 import {
   BrowserRouter as Router,
