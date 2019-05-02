06cf1767a98fd9df7a4e238b71b897b42fa710c3
just-me- <hess-marcel@bluewin.ch>
2019-05-02 19:54:33 +0200

    Eslint customer

--- src/api/customer.js
+++ src/api/customer.js
@@ -1,6 +1,8 @@
 import * as helper from './helper';
+
 export const checkResponse = helper.checkResponse;
 
+/* @noflow */
 export type Customer = {
   id?: string,
   firma?: string,
@@ -20,20 +22,20 @@ export function addCustomer(customerObject: Customer): Promise<SaveResult> {
 
 export function updateCustomer(customerObject: Customer): Promise<SaveResult> {
   return helper
-    .postJson('/kunden/' + customerObject.id, customerObject, 'PUT')
+    .postJson(`/kunden/${customerObject.id}`, customerObject, 'PUT')
     .then(helper.parseJSON);
 }
 
 export function deleteCustomer(id: string): Promise<SaveResult> {
-  return helper.deleteJson('/kunden/' + id).then(helper.parseJSON);
+  return helper.deleteJson(`/kunden/${id}`).then(helper.parseJSON);
 }
 
 export function getCustomer(id: string): Promise<Customer> {
-  return helper.getJson('/kunden/' + id).then(helper.parseJSON);
+  return helper.getJson(`/kunden/${id}`).then(helper.parseJSON);
 }
 
 export function getCustomers(deletedToo: boolean = false): Promise<{ result: Array<Customer> }> {
-  return helper.getJson('/kunden/' + (deletedToo ? 'all' : '')).then(helper.parseJSON);
+  return helper.getJson(`/kunden/${deletedToo ? 'all' : ''}`).then(helper.parseJSON);
 }
 
 export function getFilteredCustomers(
