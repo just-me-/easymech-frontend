95d08b3f145131cc8de69304fd88e9ff8ea2d728
just-me- <hess-marcel@bluewin.ch>
2019-05-06 18:55:17 +0200

    Ich probiers mal so

--- src/components/shared/getFunctions.js
+++ src/components/shared/getFunctions.js
@@ -3,18 +3,16 @@ import * as api from '../../api/customer';
 // CustomrList => get all customers
 export function getCustomers({
   filterData = undefined,
-  loadingSetter =  undefined,
-  dataSetter =  undefined,
-  fix=undefined
-}={}) {
-  console.log("There", filterData, loadingSetter, dataSetter, fix);
-  /*
-    api
-      .getFilteredCustomers(filterData)
-      .then((result) => {
-        result = api.checkResponse(result);
-        loadingSetter(false);
-        dataSetter(result);
-      })
-      .catch(error => console.log('Ups, ein Fehler ist aufgetreten', error));*/
+  loadingSetter = undefined,
+  dataSetter = undefined,
+  fix = undefined,
+} = {}) {
+  api
+    .getFilteredCustomers(filterData)
+    .then((result) => {
+      result = api.checkResponse(result);
+      loadingSetter(false);
+      dataSetter(result);
+    })
+    .catch(error => console.log('Ups, ein Fehler ist aufgetreten', error));
 }
