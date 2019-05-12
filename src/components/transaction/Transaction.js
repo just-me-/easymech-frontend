5f5260820d86acf7985c55cb0a05eee48d5e897f
udfu <dario_delpiero10@hotmail.com>
2019-05-12 14:19:22 +0200

    fix merge error which causes flow errror :( pt.2

--- src/components/transaction/Transaction.js
+++ src/components/transaction/Transaction.js
@@ -17,32 +17,34 @@ function Transaction() {
                 .addTransaction(transactionData)
                 .then((result) => {
                     result = api.checkResponse(result);
-                    NotificationManager.success("Die Transaktion wurde erfolgreich erfasst.", result.id +" erfasst");
+                    NotificationManager.success("Die Transaktion wurde erfolgreich erfasst.");
                     setKey(Math.random()); // clear data - fresh form for next entry
                 })
                 .catch(error => {
                     console.log("Ups, ein Fehler ist aufgetreten", error);
                     NotificationManager.error("Beim Speichern ist ein Fehler aufgetreten.", "Bitte erneut versuchen!");
                 });
+            console.log(transactionData);
         } else {
             NotificationManager.info("Bitte überprüfen Sie Ihre Eingaben!");
+            console.log(transactionData);
         }
     }
 
 
     return (
-    <div>
-      <Header as='h1' textAlign='center'>
-        Maschine an - und verkaufen
-      </Header>
-      <Form>
-          <TransactionFields key={key} setData={setTransactionData} setValidState={setFormIsValid} />
-          <Button primary content='Speichern' icon='save' labelPosition='left' floated='right'
-                  onClick={() => addTransaction()}
-          />
-      </Form>
-    </div>
-  )
+        <div>
+            <Header as='h1' textAlign='center'>
+                Maschine an - und verkaufen
+            </Header>
+            <Form>
+                <TransactionFields key={key} setData={setTransactionData} setValidState={setFormIsValid} />
+                <Button primary content='Speichern' icon='save' labelPosition='left' floated='right'
+                        onClick={() => addTransaction()}
+                />
+            </Form>
+        </div>
+    )
 }
 
 export default Transaction;
