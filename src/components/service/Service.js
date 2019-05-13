563e75e5244d4e5df6c7066ce9f14bbbd6aa551d
just-me- <hess-marcel@bluewin.ch>
2019-05-13 13:51:01 +0200

    Erster Zwischenstand Service Felder

--- src/components/service/Service.js
+++ src/components/service/Service.js
@@ -1,14 +1,35 @@
 // @flow
 
-import React from 'react';
-import { Header } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Button, Form, Header } from 'semantic-ui-react';
+
+import ServiceFields from './ServiceFields';
 
 function Service() {
+  const [serviceData, setServiceData] = useState({});
+  const [formIsValid, setFormIsValid] = useState(false);
+  const [key, setKey] = useState(Math.random());
+
+  function addService() {
+    console.log("2Do...");
+  }
+
   return (
     <div>
       <Header as="h1" textAlign="center">
         Dienstleistung erfassen
       </Header>
+      <Form>
+        <ServiceFields key={key} setData={setServiceData} setValidState={setFormIsValid} />
+        <Button
+          primary
+          content="Speichern"
+          icon="save"
+          labelPosition="left"
+          floated="right"
+          onClick={() => addService()}
+        />
+      </Form>
     </div>
   );
 }
---
+++ src/components/service/ServiceFields.js
@@ -0,0 +1,151 @@
+// @flow
+
+import React, { useState, useEffect } from 'react';
+import { Header, Form } from 'semantic-ui-react';
+
+import NumberInput from '../shared/NumberInput';
+import SmartInput from '../shared/SmartInput';
+
+import * as validation from '../shared/validation';
+import * as sharedCalls from '../shared/functions';
+
+import '../shared/Fields.css';
+
+// import type { Service } from '../../api/service';
+
+export type Props = {
+  data?: any, // 2do
+  setData?: any => void, // 2do
+  setValidState?: boolean => void,
+};
+
+function ServiceFields(props: Props) {
+  const initialData = {
+    id: (props.data && props.data.id) || undefined,
+    startdatum: (props.data && props.data.startdatum) || '',
+    enddatum: (props.data && props.data.enddatum) || '',
+    maschinenId: (props.data && props.data.maschinenId) || '',
+    kundenId: (props.data && props.data.kundenId) || '',
+  };
+
+  const [serviceData, setServiceData] = useState(initialData);
+  const [customerData, setCustomerData] = useState();
+  const [machineData, setMachineData] = useState();
+
+  const [datesAreValid, setDatesAreValid] = useState({
+    startdatum: true,
+    enddatum: true,
+  });
+
+  function handleMachineSelect(result) {
+    setServiceData({ ...serviceData, maschinenId: result.id });
+  }
+
+  function handleCustomerSelect(result) {
+    setServiceData({ ...serviceData, kundenId: result.id });
+  }
+
+  function handleChange(element, { validate }) {
+    let value = element.target.value;
+    if (validate) {
+      if (validate === 'number') {
+        value = validation.toNumber(value);
+      }
+      if (validate === 'date') {
+        value = validation.toDate(value);
+        if (props.setValidState) {
+          setDatesAreValid({
+            ...datesAreValid,
+            [element.target.id]: value ? validation.checkDate(value) : true,
+          });
+        }
+      }
+    }
+    setServiceData({ ...serviceData, [element.target.id]: value });
+  }
+
+  useEffect(() => {
+    const requiredIsValide = Object.values(datesAreValid).every(val => val === true)
+      && parseInt(serviceData.maschinenId, 10) > 0
+      && parseInt(serviceData.kundenId, 10) > 0;
+    if (props.setValidState) {
+      props.setValidState(requiredIsValide);
+    }
+    if (props.setData) {
+      props.setData(serviceData);
+    }
+  });
+
+  useEffect(() => {
+    sharedCalls.getCustomers({
+      deletedToo: true,
+      dataSetter: setCustomerData,
+    });
+    sharedCalls.getMachines({
+      deletedToo: true,
+      dataSetter: setMachineData,
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className="Form-section">
+        <Form.Group widths="equal">
+          <NumberInput
+            id="startdatum"
+            label="Startdatum"
+            innerLabel="DD.MM.YYYY"
+            value={serviceData.startdatum}
+            validate="date"
+            handleChange={handleChange}
+            error={!datesAreValid.startdatum}
+          />
+          <NumberInput
+            id="enddatum"
+            label="Enddatum"
+            innerLabel="DD.MM.YYYY"
+            value={serviceData.enddatum}
+            validate="date"
+            handleChange={handleChange}
+            error={!datesAreValid.enddatum}
+          />
+        </Form.Group>
+
+        <Form.Group widths="equal">
+          <SmartInput
+            id="maschine"
+            label="Maschine"
+            matchingKey="seriennummer"
+            onResultSelect={handleMachineSelect}
+            elements={machineData}
+            setElementId={props.data ? props.data.maschinenId : 0}
+            noResultsMessage="Keine Maschine gefunden"
+            isRequired
+          />
+          <SmartInput
+            id="kunde"
+            label="Kunde"
+            matchingKey="firma"
+            onResultSelect={handleCustomerSelect}
+            elements={customerData}
+            setElementId={props.data ? props.data.kundenId : 0}
+            noResultsMessage="Kein Kunden gefunden"
+            isRequired
+          />
+        </Form.Group>
+      </div>
+
+      <Header as="h2">Material</Header>
+      <div className="Form-section">
+
+      </div>
+
+      <Header as="h2">Arbeit</Header>
+      <div className="Form-section">
+
+      </div>
+    </div>
+  );
+}
+
+export default ServiceFields;
