85c6428ab027823d289183adcd7c435c5d61bc34
just-me- <hess-marcel@bluewin.ch>
2019-05-15 21:20:44 +0200

    Erstmals formatieren... #wtf xD

--- src/components/service/SearchResult.js
+++ src/components/service/SearchResult.js
@@ -1,20 +1,19 @@
 // @flow
 import React from 'react';
-import {Header} from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 
 export type Props = {
-    filterData: ?any,
+  filterData: ?any,
 };
 
 function SearchResult(props: Props) {
-
-    return (
-        <div>
-            <Header as="h1" textAlign="center">
-                Suchresultate
-            </Header>
-        </div>
-    );
+  return (
+    <div>
+      <Header as="h1" textAlign="center">
+        Suchresultate
+      </Header>
+    </div>
+  );
 }
 
 export default SearchResult;
--- src/components/service/ServiceSearchFields.js
+++ src/components/service/ServiceSearchFields.js
@@ -1,180 +1,173 @@
 // @flow
 
-import React, {useState, useEffect} from 'react'
-import {Form} from 'semantic-ui-react'
+import React, { useState, useEffect } from 'react';
+import { Form } from 'semantic-ui-react';
 import SmartInput from '../shared/SmartInput';
 
-import * as helper from "../shared/functions"
-import * as validation from "../shared/validation"
-import type {Transaction} from "../../api/transaction";
+import * as helper from '../shared/functions';
+import * as validation from '../shared/validation';
+import type { Transaction } from '../../api/transaction';
 import type { TypeMachine } from '../../api/machine';
-import "../shared/Fields.css"
-import NumberInput from "../shared/NumberInput";
-import Checkbox from "semantic-ui-react/dist/commonjs/modules/Checkbox/Checkbox";
-
+import '../shared/Fields.css';
+import NumberInput from '../shared/NumberInput';
+import Checkbox from 'semantic-ui-react/dist/commonjs/modules/Checkbox/Checkbox';
 
 export type Props = {
-    data?: Transaction,
-    setData?: (Transaction) => void,
-    setValidState?: (boolean) => void,
-    searchView?: boolean,
-    machine?: TypeMachine
+  data?: Transaction,
+  setData?: Transaction => void,
+  setValidState?: boolean => void,
+  searchView?: boolean,
+  machine?: TypeMachine,
 };
 
 function ServiceSearchFields(props: Props) {
-    const initialData = {
-        maschinenId: (props.data && props.data.maschineId) || '',
-        kundenId: (props.data && props.data.kundenId) || '',
-        maschinentypId: (props.data && props.data.maschinentypId) || '',
-        startdatum: (props.data && props.data.startdatum) || '',
-        enddatum: (props.data && props.data.enddatum) || '',
-        searchTransaction: (props.data && props.data.searchTransaction) || false,
-        searchRental: (props.data && props.data.searchRental) || false,
-        searchService: (props.data && props.data.searchService) || false,
-    };
+  const initialData = {
+    maschinenId: (props.data && props.data.maschineId) || '',
+    kundenId: (props.data && props.data.kundenId) || '',
+    maschinentypId: (props.data && props.data.maschinentypId) || '',
+    startdatum: (props.data && props.data.startdatum) || '',
+    enddatum: (props.data && props.data.enddatum) || '',
+    searchTransaction: (props.data && props.data.searchTransaction) || false,
+    searchRental: (props.data && props.data.searchRental) || false,
+    searchService: (props.data && props.data.searchService) || false,
+  };
 
-    const [searchData, setSearchData] = useState(initialData);
-    const [customerData, setCustomerData] = useState();
-    const [machineData, setMachineData] = useState();
-    const [machineTypeData, setMachineTypeData] = useState();
+  const [searchData, setSearchData] = useState(initialData);
+  const [customerData, setCustomerData] = useState();
+  const [machineData, setMachineData] = useState();
+  const [machineTypeData, setMachineTypeData] = useState();
 
-    function handleMachineSelect(result) {
-        setSearchData({ ...searchData, maschinenId: result.id });
+  function handleMachineSelect(result) {
+    setSearchData({ ...searchData, maschinenId: result.id });
+  }
+
+  function handleCustomerSelect(result) {
+    setSearchData({ ...searchData, kundenId: result.id });
+  }
+
+  function handleMachineTypeSelect(result) {
+    setSearchData({ ...searchData, maschinentypId: result.id });
+  }
+
+  function handleChange(element, { validate }) {
+    let value = element.target.value;
+    switch (validate) {
+      case 'number':
+        value = validation.toNumber(value);
+        break;
+      case 'date':
+        value = validation.toDate(value);
+        break;
+      default:
+        break;
     }
+    setSearchData({ ...searchData, [element.target.id]: value });
+  }
+  function handleCheckBox(element, { checked }) {
+    setSearchData({ ...searchData, [element.target.id]: checked });
+    console.log(checked);
+  }
 
-    function handleCustomerSelect(result) {
-        setSearchData({ ...searchData, kundenId: result.id });
+  useEffect(() => {
+    if (props.setData) {
+      props.setData(searchData);
     }
+  });
 
-    function handleMachineTypeSelect(result) {
-        setSearchData({ ...searchData, maschinentypId: result.id });
-    }
-
-
-    function handleChange(element, { validate }) {
-        let value = element.target.value;
-        switch (validate) {
-            case 'number':
-                value = validation.toNumber(value);
-                break;
-            case 'date':
-                value = validation.toDate(value);
-                break;
-            default:
-                break;
-        }
-        setSearchData({ ...searchData, [element.target.id]: value });
-    }
-    function handleCheckBox(element, { checked }){
-        setSearchData({ ...searchData, [element.target.id]: checked });
-        console.log(checked)
-    }
-
-    useEffect(() => {
-
-        if (props.setData) {
-            props.setData(searchData);
-        }
+  useEffect(() => {
+    helper.getCustomers({
+      deletedToo: true,
+      dataSetter: setCustomerData,
     });
+    helper.getMachines({
+      deletedToo: true,
+      dataSetter: setMachineData,
+    });
+    helper.getMachinetypes({
+      dataSetter: setMachineTypeData,
+    });
+  }, []);
 
-    useEffect(() => {
-        helper.getCustomers({
-            deletedToo: true,
-            dataSetter: setCustomerData,
-        });
-        helper.getMachines({
-            deletedToo: true,
-            dataSetter: setMachineData,
-        });
-        helper.getMachinetypes({
-            dataSetter: setMachineTypeData,
-        });
-    }, []);
-
-    return (
-        <div>
-            <div className="Form-section">
-                <Form.Group widths='equal'>
-                    <SmartInput
-                        id="maschine"
-                        label="Maschinen"
-                        matchingKey="seriennummer"
-                        onResultSelect={handleMachineSelect}
-                        elements={machineData}
-                        setElementId={props.data ? props.data.maschinenid : 0}
-                        noResultsMessage="Keine Maschinen gefunden"
-                    />
-                    <SmartInput
-                        id="maschinentyp"
-                        label="Maschinentyp"
-                        matchingKey="fabrikat"
-                        onResultSelect={handleMachineTypeSelect}
-                        elements={machineTypeData}
-                        setElementId={props.data ? props.data.maschinentypId : 0}
-                        noResultsMessage="Keine Maschinentypen gefunden"
-                        isRequired={!props.searchView}
-                    />
-                </Form.Group>
-                <Form.Group widths='equal'>
-                    <SmartInput
-                        id="kunde"
-                        label="Kunde"
-                        matchingKey="firma"
-                        onResultSelect={handleCustomerSelect}
-                        elements={customerData}
-                        setElementId={props.data ? props.data.kundenid : 0}
-                        noResultsMessage="Keine Kunden gefunden"
-                    />
-                    <Form.Input
-                        label="Dummy"
-                        className="dummyObject"
-                        placeholder="Dummy Placeholder for equal dividing"
-                    />
-                </Form.Group>
-                <Form.Group widths='equal'>
-                    <NumberInput
-                        id="startdatum"
-                        label="Startdatum - Suchzeitraum"
-                        innerLabel="DD.MM.YYYY"
-                        value={searchData.startdatum}
-                        validate="date"
-                        handleChange={handleChange}
-                        error={!searchData}
-                    />
-                    <NumberInput
-                        id="enddatum"
-                        label="Enddatum - Suchzeitraum"
-                        innerLabel="DD.MM.YYYY"
-                        value={searchData.enddatum}
-                        validate="date"
-                        handleChange={handleChange}
-                        error={!searchData}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Checkbox
-                        id='searchTransaction'
-                        label='Ankauf oder Verkauf durchsuchen'
-                        onClick={handleCheckBox}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Checkbox
-                        id='searchRental'
-                        label='Vermietungen durchsuchen'
-                        onClick={handleCheckBox}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Checkbox
-                        id='searchService'
-                        label='Dienstleistungen durchsuchen'
-                        onClick={handleCheckBox}
-                    />
-                </Form.Group>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="Form-section">
+        <Form.Group widths="equal">
+          <SmartInput
+            id="maschine"
+            label="Maschinen"
+            matchingKey="seriennummer"
+            onResultSelect={handleMachineSelect}
+            elements={machineData}
+            setElementId={props.data ? props.data.maschinenid : 0}
+            noResultsMessage="Keine Maschinen gefunden"
+          />
+          <SmartInput
+            id="maschinentyp"
+            label="Maschinentyp"
+            matchingKey="fabrikat"
+            onResultSelect={handleMachineTypeSelect}
+            elements={machineTypeData}
+            setElementId={props.data ? props.data.maschinentypId : 0}
+            noResultsMessage="Keine Maschinentypen gefunden"
+            isRequired={!props.searchView}
+          />
+        </Form.Group>
+        <Form.Group widths="equal">
+          <SmartInput
+            id="kunde"
+            label="Kunde"
+            matchingKey="firma"
+            onResultSelect={handleCustomerSelect}
+            elements={customerData}
+            setElementId={props.data ? props.data.kundenid : 0}
+            noResultsMessage="Keine Kunden gefunden"
+          />
+          <Form.Input
+            label="Dummy"
+            className="dummyObject"
+            placeholder="Dummy Placeholder for equal dividing"
+          />
+        </Form.Group>
+        <Form.Group widths="equal">
+          <NumberInput
+            id="startdatum"
+            label="Startdatum - Suchzeitraum"
+            innerLabel="DD.MM.YYYY"
+            value={searchData.startdatum}
+            validate="date"
+            handleChange={handleChange}
+            error={!searchData}
+          />
+          <NumberInput
+            id="enddatum"
+            label="Enddatum - Suchzeitraum"
+            innerLabel="DD.MM.YYYY"
+            value={searchData.enddatum}
+            validate="date"
+            handleChange={handleChange}
+            error={!searchData}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Checkbox
+            id="searchTransaction"
+            label="Ankauf oder Verkauf durchsuchen"
+            onClick={handleCheckBox}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Checkbox id="searchRental" label="Vermietungen durchsuchen" onClick={handleCheckBox} />
+        </Form.Group>
+        <Form.Group>
+          <Checkbox
+            id="searchService"
+            label="Dienstleistungen durchsuchen"
+            onClick={handleCheckBox}
+          />
+        </Form.Group>
+      </div>
+    </div>
+  );
 }
 
-export default ServiceSearchFields
+export default ServiceSearchFields;
