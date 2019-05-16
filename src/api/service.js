5f236bfe2e2847ae60506c8f6b47aeeaa4c1f97f
udfu <dario_delpiero10@hotmail.com>
2019-05-16 17:27:54 +0200

    added api calls for service

--- src/api/service.js
+++ src/api/service.js
@@ -10,7 +10,7 @@ export type Material ={
     serviceId?: number
 }
 
-export type Workstep = {
+export type WorkStep = {
     id?: number,
     bezeichnung?: string,
     stundensatz?: string,
@@ -26,7 +26,37 @@ export type Service = {
     maschinenId?: number,
     kundenId?: number,
     materialposten?: Array<Material>,
-    arbeitsschritte?: Array<Workstep>
+    arbeitsschritte?: Array<WorkStep>
 };
 
+function prepareDto(serviceObject: Service) {
+    const dateTypes = ['beginn', 'ende'];
+    let service = JSON.parse(JSON.stringify(serviceObject));
+    service = helper.convertToDatabaseDates(service, dateTypes);
+    return service;
+}
 
+export function addService(serviceObject: Service): Promise<SaveResult> {
+    return helper
+        .postJson('/services/', prepareDto(serviceObject), 'POST')
+        .then(helper.parseJSON);
+}
+
+export function updateService(serviceObject: Service): Promise<SaveResult> {
+    return helper
+        .postJson(`/services/${serviceObject.id}`, prepareDto(serviceObject), 'PUT')
+        .then(helper.parseJSON);
+}
+
+export function deleteService(id: string): Promise<SaveResult> {
+    return helper.deleteJson(`/services/${id}`).then(helper.parseJSON);
+}
+
+/* not sure if  needed - in case of no usage -> could be deleted*/
+export function getService(id: string): Promise<Service> {
+    return helper.getJson(`/services/${id}`).then(helper.parseJSON);
+}
+
+export function getServices(): Promise<{ result: Array<Service> }> {
+    return helper.getJson('/services/').then(helper.parseJSON);
+}
