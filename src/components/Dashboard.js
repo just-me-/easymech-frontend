b9f1123e13af5225014a02a46348cfb7bdb13b7a
just-me- <hess-marcel@bluewin.ch>
2019-05-20 13:48:38 +0200

    Zwischenstand

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,77 @@
 // @flow
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from 'semantic-ui-react';
 
+import * as sharedCalls from './shared/functions';
+
+import ServiceSearchList from './service/ServiceSearchList';
+import RentalSearchList from './transaction/RentalSearchList';
+
 function Dashboard() {
+  const [machineData, setMachineData] = useState([]);
+  const [customerData, setCustomerData] = useState([]);
+
+  function getCustomerText(id: number) {
+    if (id) {
+      const customer = customerData.find(x => x.id === id);
+      if (customer) {
+        return customer.firma;
+      }
+      return 'Nicht gefunden';
+    }
+    return 'Kunde hinterlegt';
+  }
+
+  function getMachineText(id: number) {
+    if (id) {
+      const machine = machineData.find(x => x.id === id);
+      if (machine) {
+        return machine.seriennummer;
+      }
+      return 'Nicht gefunden';
+    }
+    return 'Keine Maschine hinterlegt';
+  }
+
+  useEffect(() => {
+    sharedCalls.getCustomers({
+      deletedToo: true,
+      dataSetter: setCustomerData,
+    });
+
+    sharedCalls.getMachines({
+      deletedToo: true,
+      dataSetter: setMachineData,
+    });
+  }, []);
+
   return (
-    <Header as="h1" textAlign="center">
-      Dashboard
-    </Header>
+    <React.Fragment>
+      <Header as="h1" textAlign="center">
+        Aktuelles in der Übersicht
+      </Header>
+
+      <Header as="h2">Anstehende Reparaturen und Servicearbeiten</Header>
+      <ServiceSearchList
+        filterData={{}}
+        resolveCustomer={getCustomerText}
+        resolveMachine={getMachineText}
+        title="Gefundene Dienstleistungen"
+      />
+
+      <Header as="h2">Laufende Reparaturen und Servicearbeiten</Header>
+
+      <Header as="h2">Aktuelle Vermietungen - ausgehende Maschinen</Header>
+      <RentalSearchList
+        filterData={{}}
+        resolveCustomer={getCustomerText}
+        resolveMachine={getMachineText}
+        title="Gefundene Reservationen"
+      />
+
+      <Header as="h2">Aktuelle Vermitungen - eingehende Maschinen</Header>
+    </React.Fragment>
   );
 }
 
